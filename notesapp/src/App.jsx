e88c7a8bed2367ef0773e16a73d1fe9e72d727ca
Mayank Thole <mayank.thole-i@Transbnk.com>
2026-02-19 15:36:29 +0530

    making new changes

--- notesapp/src/App.jsx
+++ notesapp/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <img src={reactLogo} className="logo react" alt="React logo" />
         <h1>Hello from Amplify</h1>
+        <h2>We will explore more options</h2>
       </header>
     </div>
   );
